0dddc0f78568b5b4969a6f47008e108d8c54272e
Shyaken <shyaken@live.com>
2014-10-09 06:00:11 +0700

    first commit

---
+++ application/views/common/footer.php
@@ -0,0 +1,42 @@
+<div id="footer-text">
+            <div class="container">
+                <div class="sponsor seperate">
+                  <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                      <img src="img/sponsor-logo-1.png" alt="...">
+                    </a>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                      <img src="img/sponsor-logo-1.png" alt="...">
+                    </a>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                      <img src="img/sponsor-logo-1.png" alt="...">
+                    </a>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                      <img src="img/sponsor-logo-1.png" alt="...">
+                    </a>
+                  </div>
+                </div>
+            </div>
+            <div class="wide seperate">
+                
+            </div>
+            <div class="container">
+                <div class="left">
+                    <a href="#" class="">About Us</a>
+                    <a href="#" class="">Term and Conditions</a>
+                    <a href="#" class="">Privacy</a>
+                    <a href="#" class="">Contact</a>
+                </div>
+                <div class="right"> 
+                    Copyright © 2014 <span class="color_h1">UnityAppStudio</span>. All Rights Reserved.
+                </div> <!-- end right -->
+            </div>   <!-- end container -->
+        </div> <!-- end footer -->
+    </body>
+</html>
